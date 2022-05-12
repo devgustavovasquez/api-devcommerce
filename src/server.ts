7baf406ce2af4879c00099b96cce5a8a7f88ecb4
Tafuso <gustavovasquez2002@gmail.com>
2022-05-12 17:08:06 -0300

    initial config with: typescript, express, jest, eslint and prettier

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const port = process.env.PORT || 3333;
+const app = express();
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server is running 🚀`));
