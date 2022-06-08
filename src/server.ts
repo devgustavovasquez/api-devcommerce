2b4dab900b1367278d804d25f31cc440dc8d5c3e
Tafuso <gustavovasquez2002@gmail.com>
2022-06-08 12:36:56 -0300

    chore: install and config cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes/routes';
 
 const port = process.env.PORT || 3333;
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
